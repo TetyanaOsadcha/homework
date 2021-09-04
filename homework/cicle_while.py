2d62d87388009a31a3c1437498c4aa75a5ecc7cd
TetyanaOsadcha <90113900+TetyanaOsadcha@users.noreply.github.com>
2021-09-05 00:06:13 +0300

    Update cicle_while.py

---
+++ homework/cicle_while.py
@@ -0,0 +1,15 @@
+def trio(x,y,z):
+    if x == 0 or y == 0 or z == 0:
+        print('Введите число не равное 0!!!')
+    else:
+        while x<y or y == x:
+             print(x, '<', y)
+             x=x+z
+            # if x>y:
+             continue
+        print('Bingo!!!', x, '>', y)
+
+a=int(input('Введите число А'))
+b=int(input('Введите число B'))
+c=int(input('Введите число C'))
+trio(a,b,c)
